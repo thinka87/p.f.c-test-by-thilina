e8e3e62cbf8096ea8666e223f6578d87b7f461e5
thinka87 <thinkaemoney@gmail.com>
2022-04-15 20:46:56 +0530

    create validate-iban post end point

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_restful import Api
+from IBANValidator import IBANValidator
 
-app = Flask(__name__)
+app = Flask(__name__) # Get flask app instance
+api = Api(app) # Get flask_restful Api instance
 
+api.add_resource(IBANValidator,'/validate-iban') # Add post request handle to validate iban number
 
 if __name__ == "__main__":
     app.run()
