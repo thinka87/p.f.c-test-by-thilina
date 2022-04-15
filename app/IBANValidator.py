a8bae7a68b7e23fa20011a975be42e3248fe89ab
thinka87 <thinkaemoney@gmail.com>
2022-04-15 21:17:26 +0530

    Sanitze iban number

--- app/IBANValidator.py
+++ app/IBANValidator.py
@@ -1,3 +1,5 @@
+import re
+from time import strptime
 from flask_restful import Resource
 from flask_restful import reqparse
 
@@ -8,3 +10,19 @@ class IBANValidator(Resource):
             parser = reqparse.RequestParser()
             parser.add_argument('iban_number', required=True, type=str, help='iban_number is required')
             args = parser.parse_args()
+            return self._sanitize_iban_number(args["iban_number"])
+
+    def _sanitize_iban_number(self,iban_number):
+
+        iban_number = iban_number.replace(" ", "")
+        iban_number =iban_number.upper()
+        iban_number =self._remove_prefix(iban_number,"IBAN")
+        iban_number = ''.join(filter(str.isalnum, iban_number))
+
+        return iban_number
+
+    def _remove_prefix(self,text, prefix):
+
+        if text.startswith(prefix):
+            text = text.replace(prefix, "", 1)
+        return text
