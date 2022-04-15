a209aeb7b43ef143ba4af6ca3a5b57299013b8ac
thinka87 <thinkaemoney@gmail.com>
2022-04-16 00:52:37 +0530

    Complete basic validation

--- app/IBANValidator.py
+++ app/IBANValidator.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource
 from flask_restful import reqparse
 import json
+import re
 
 class IBANValidator(Resource):
 
@@ -17,6 +18,13 @@ class IBANValidator(Resource):
             
             if self._validate_iban_length(iban_number,int(is_valid_country["iban_length"])) is False:
                 return {"message" : "Invalid IBAN length","code" : "INVALID_IBAN_LENGTH" }, 400
+            
+            if self._validate_iban_format(iban_number,is_valid_country["iban_format_regex"]) is False:
+                return {"message" : "Invalid IBAN format","code" : "INVALID_IBAN_FORMAT" }, 400
+            
+            if self._validate_iban_checksum(iban_number) is False:
+                return {"message" : "Invalid IBAN checksum value","code" : "INVALID_IBAN_CHECKSUM" }, 400
+
 
     def _sanitize_iban_number(self,iban_number):
 
@@ -52,3 +60,26 @@ class IBANValidator(Resource):
             return True
         
         return False
+    
+    def _validate_iban_format(self,iban_number,regx_format):
+
+        iban_format = re.compile(regx_format, re.I)
+        return bool(iban_format.match(iban_number))
+
+
+    def _validate_iban_checksum(self,iban_number):
+
+        iban_number_formated = iban_number[:2] + '00' + iban_number[4:]
+        calcualted_check_digit=self._get_iban_numeric_value(iban_number_formated)
+        format_check_digit='{:0>2}'.format(98 - (int(calcualted_check_digit) % 97))
+        iban_numeric_value= int(self._get_iban_numeric_value(iban_number))
+
+        if format_check_digit == iban_number[2:4] and  iban_numeric_value% 97 == 1:
+            return True
+        
+        return False
+
+
+    def _get_iban_numeric_value(Self,iban_number):
+
+            return  re.sub('[A-Z]', lambda m: str(ord(m.group()) - 55), (iban_number[4:] + iban_number[:4]))
