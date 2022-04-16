38d79804e75e3f527dec24ab7b40ee98219cd626
thinka87 <thinkaemoney@gmail.com>
2022-04-16 14:14:31 +0530

    Custom 404 handler

--- app/main.py
+++ app/main.py
@@ -7,5 +7,9 @@ api = Api(app) # Get flask_restful Api instance
 
 api.add_resource(IBANValidator,'/validate-iban') # Add post request handle to validate iban number
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return {"message": e.description}, e.code
+
 if __name__ == "__main__":
     app.run()
